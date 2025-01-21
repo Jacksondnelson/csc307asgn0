7c8c29fcda26c9e7917d13bf78a20bc3e5cb3932
jacksonnelson <jacksondnelson10@gmail.com>
2025-01-21 09:59:32 -0800

    added delete method

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -78,6 +78,17 @@ app.post("/users", (req, res) => {
   res.send();
 });
 
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    const location = users.users_list.findIndex(user => user.id == id);
+    users.users_list.splice(location, 1);
+  }
+});
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
